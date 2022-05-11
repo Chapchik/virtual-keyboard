c57e604840ffcf32621fe8de2922861c305fc11f
Dmytro Chernukha <chapchik@icloud.com>
2022-05-11 09:38:37 +0300

    fix: fix linter errors

--- app/src/index.js
+++ app/src/index.js
@@ -2,44 +2,13 @@ import createWrapper from './html_tags.js';
 
 createWrapper();
 
-const buttonContentEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 
-'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 
-'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift', 'z', 
-'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 
-'Alt', '←', '↓', '→', 'Ctrl'];
-
-const buttonContentRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 
-'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 
-'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 
-'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 
-'Alt', '←', '↓', '→', 'Ctrl'];
-
-const buttonShiftEn = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 
-'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 
-'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 
-'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 
-'Alt', '←', '↓', '→', 'Ctrl'];
-
-const buttonShiftRu = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 
-'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del', 
-'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 
-'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 
-'Alt', '←', '↓', '→', 'Ctrl'];
-
-const buttonCapsEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 
-'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del', 
-'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'Enter', 'Shift', 'Z', 
-'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 
-'Alt', '←', '↓', '→', 'Ctrl'];
-
-const buttonCapsRu = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 
-'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Del', 
-'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 
-'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 
-'Alt', '←', '↓', '→', 'Ctrl'];
-
-const buttonWork = ['Backspace', 'Tab', 'Del', 'CapsLock', 'Enter', 'Shift', '↑', 'Shift', 
-'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+const buttonContentEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+const buttonContentRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+const buttonShiftEn = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+const buttonShiftRu = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+const buttonCapsEn = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+const buttonCapsRu = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Del', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+const buttonWork = ['Backspace', 'Tab', 'Del', 'CapsLock', 'Enter', 'Shift', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
 let language = localStorage.getItem('language') || 'en';
 let flag = true;
 
@@ -74,7 +43,6 @@ const arrowLeft = document.querySelector('.←');
 const win = document.querySelector('.win');
 const del = document.querySelector('.del');
 
-
 function changeButton(arr) {
   for (let i = 0; i < arr.length; i += 1) {
     buttons[i].innerHTML = arr[i];
@@ -82,11 +50,10 @@ function changeButton(arr) {
 }
 
 document.addEventListener('keydown', (e) => {
-  console.log(e)
   if (e.key === 'Shift' && language === 'en') {
-    changeButton(buttonShiftEn)
+    changeButton(buttonShiftEn);
   } else if (e.key === 'Shift' && language === 'ru') {
-    changeButton(buttonShiftRu)
+    changeButton(buttonShiftRu);
   }
 
   if (e.key === 'CapsLock' && language === 'en' && flag) {
@@ -130,7 +97,7 @@ document.addEventListener('keydown', (e) => {
     ctrl.forEach((item) => {
       item.classList.add('click');
       setTimeout(() => { item.classList.remove('click'); }, 300);
-    })
+    });
   }
 
   if (e.key === 'ArrowUp') {
@@ -166,9 +133,9 @@ document.addEventListener('keydown', (e) => {
 
 document.addEventListener('keyup', (e) => {
   if (e.key === 'Shift' && language === 'en') {
-    changeButton(buttonContentEn)
+    changeButton(buttonContentEn);
   } else if (e.key === 'Shift' && language === 'ru') {
-    changeButton(buttonContentRu)
+    changeButton(buttonContentRu);
   }
 });
 
@@ -183,7 +150,6 @@ buttons.forEach((item) => {
     } else if (item.innerHTML === 'Space') {
       textArea.value += ' ';
     } else if (item.innerHTML === 'CapsLock') {
-      
       if (language === 'en' && flag) {
         changeButton(buttonCapsEn);
         capslock.classList.add('active');
@@ -213,22 +179,20 @@ buttons.forEach((item) => {
 
 buttons.forEach((item) => {
   item.addEventListener('mousedown', () => {
-    if (item.innerHTML === 'Shift' && language === 'en') { 
-          changeButton(buttonShiftEn)
-      } else if (item.innerHTML === 'Shift' && language === 'ru') {
-          changeButton(buttonShiftRu)
-        }
-    }  
-  );
+    if (item.innerHTML === 'Shift' && language === 'en') {
+      changeButton(buttonShiftEn);
+    } else if (item.innerHTML === 'Shift' && language === 'ru') {
+      changeButton(buttonShiftRu);
+    }
+  });
 });
 
 buttons.forEach((item) => {
   item.addEventListener('mouseup', () => {
-    if (item.innerHTML === 'Shift' && language === 'en') { 
-        changeButton(buttonContentEn)
+    if (item.innerHTML === 'Shift' && language === 'en') {
+      changeButton(buttonContentEn);
     } else if (item.innerHTML === 'Shift' && language === 'ru') {
-        changeButton(buttonContentRu)
-      }
-    }  
-  );
+      changeButton(buttonContentRu);
+    }
+  });
 });
